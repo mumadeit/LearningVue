6d4a643f57946d294884b4d3b9566e35afe120ba
Muayid Ahmed <moahfox@gmail.com>
2024-01-30 17:17:36 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import bootstrap from 'bootstrap'
+
+</script>
+
+<template>
+  <header class="p-3 text-bg-light border">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <RouterLink class="nav-link text-dark px-2" to="/">Home</RouterLink>
+          <RouterLink class="nav-link text-dark px-2" to="/about">About</RouterLink>
+        </ul>
+
+
+
+        <div class="text-end">
+          <button type="button" class="btn-main me-2">Let's Talk</button>
+          <button type="button" class="btn-main">Call me</button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container pt-5">
+    <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+      <div class="col-lg-8 px-0">
+        <h1 class="display-4">About me</h1>
+        <p class="lead my-3">I'm Muayid Ahmed Al-Owaisi, a high-end freelancer specializing in web development and digital
+          marketing. I provide comprehensive solutions, encompassing everything from web services to style guides, all
+          tailored to meet your unique requirements. My dedication to innovation and meticulous attention to detail
+          enables me to transform your visions into captivating websites and distinctive brand identities, supporting you
+          at every stage of the creative journey. </p>
+        <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Continue reading...</a></p>
+      </div>
+    </div>
+  </main>
+</template>
+
+
